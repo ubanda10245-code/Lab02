7ebd4ea8ae9f3f9bdff687b78eaeb84270fb79c9
Joseph Ubanda-Ruiz <jubanda@ku.edu>
2026-02-04 13:51:58 -0600

    Add C++ files and Makefile

---
+++ functions.h
@@ -0,0 +1,6 @@
+/* functions.h */
+#if !defined(_FUNC_H_)
+#define _FUNC_H_
+void print_hello();
+int factorial(int n);
+#endif /* if !define(_FUNC_H_) */ 
---
+++ hello.cpp
@@ -0,0 +1,8 @@
+/* hello.cpp */
+#include <iostream>
+#include "functions.h"
+using namespace std;
+void print_hello()
+{
+cout << "Hello World!";
+}
